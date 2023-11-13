ac500df7247b1461c70c6f52fd2cfa4274c1d9d3
Angus Dippenaar <angusdippenaar@gmail.com>
2023-11-13 07:02:04 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Gets you frogs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+  };
+
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    devshell,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        devshell.flakeModule
+        flake-parts.flakeModules.easyOverlay
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, pkgs, system, ... }: {
+        # Attrs for easyOverlay
+        overlayAttrs = {
+          inherit (config.packages)
+            nodeCrawler
+            nodeCrawlerFrontend;
+        };
+
+        devshells.default = {
+          packages = with pkgs; [
+            bun
+          ];
+        };
+      };
+  };
+}
